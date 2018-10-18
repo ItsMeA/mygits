859c1498c31bc0a8f8ad00e167aa511aaaa7e65b
liyan <1539230902@qq.com>
2018-10-18 18:29:37 +0800

    3-4

--- imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package com.imooc;
 
 import org.springframework.boot.SpringApplication;
@@ -5,18 +8,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * @author zhailiang
+ *
+ */
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class DemoApplication {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
 	@GetMapping("/hello")
-	public String hello(){
-		return "spring hello world";
+	public String hello() {
+		return "hello spring security";
 	}
 
 }
